e0f94e560d972f3c38b963ff5de87b5603c605ac
Maldu <anamld62@gmail.com>
2024-12-09 16:28:13 +0100

    class TextAugmentation created

--- notebooks/experimentation/experiments_utils.py
+++ notebooks/experimentation/experiments_utils.py
@@ -1,5 +1,9 @@
 import yaml
 import os
+import random
+from random import shuffle
+random.seed(1)
+
 
 def experiment_status(config_file='experiments_config.yaml'):
     """
@@ -22,3 +26,136 @@ def experiment_status(config_file='experiments_config.yaml'):
     experiment_tags = config['experiment_tags']
     
     return experiment_name, experiment_description, experiment_tags
+
+
+from nltk.corpus import wordnet 
+
+class TextAugmentation:
+    def __init__(self, alpha_sr=0.1, alpha_ri=0.1, alpha_rs=0.1, p_rd=0.1, num_aug=9):
+        self.alpha_sr = alpha_sr  
+        self.alpha_ri = alpha_ri  
+        self.alpha_rs = alpha_rs  
+        self.p_rd = p_rd          
+        self.num_aug = num_aug    
+
+    def synonym_replacement(self, words, n):
+        new_words = words.copy()
+        random_word_list = list(set(words))
+        random.shuffle(random_word_list)
+        num_replaced = 0
+
+        for random_word in random_word_list:
+            synonyms = self.get_synonyms(random_word)
+            if len(synonyms) >= 1:
+                synonym = random.choice(list(synonyms))
+                new_words = [synonym if word == random_word else word for word in new_words]
+                # print("replaced", random_word, "with", synonym)
+                num_replaced += 1
+            if num_replaced >= n:
+                break
+
+        sentence = ' '.join(new_words)
+        new_words = sentence.split(' ')
+        return new_words
+
+    def get_synonyms(self, word):
+        synonyms = set()
+        for syn in wordnet.synsets(word): 
+            for l in syn.lemmas(): 
+                synonym = l.name().replace("_", " ").replace("-", " ").lower()
+                synonym = "".join([char for char in synonym if char in ' qwertyuiopasdfghjklzxcvbnm'])
+                synonyms.add(synonym) 
+        if word in synonyms:
+            synonyms.remove(word)
+        return list(synonyms)
+
+    def random_deletion(self, words, p):
+        if len(words) == 1:
+            return words
+        new_words = []
+        for word in words:
+            r = random.uniform(0, 1)
+            if r > p:
+                new_words.append(word)
+        if len(new_words) == 0:
+            rand_int = random.randint(0, len(words)-1)
+            return [words[rand_int]]
+        return new_words
+
+    def random_swap(self, words, n):
+        new_words = words.copy()
+        for _ in range(n):
+            new_words = self.swap_word(new_words)
+        return new_words
+
+    def swap_word(self, new_words):
+        random_idx_1 = random.randint(0, len(new_words)-1)
+        random_idx_2 = random_idx_1
+        counter = 0
+        while random_idx_2 == random_idx_1:
+            random_idx_2 = random.randint(0, len(new_words)-1)
+            counter += 1
+            if counter > 3:
+                return new_words
+        new_words[random_idx_1], new_words[random_idx_2] = new_words[random_idx_2], new_words[random_idx_1] 
+        return new_words
+
+    def random_insertion(self, words, n):
+        new_words = words.copy()
+        for _ in range(n):
+            self.add_word(new_words)
+        return new_words
+
+    def add_word(self, new_words):
+        synonyms = []
+        counter = 0
+        while len(synonyms) < 1:
+            random_word = new_words[random.randint(0, len(new_words)-1)]
+            synonyms = self.get_synonyms(random_word)
+            counter += 1
+            if counter >= 10:
+                return
+        random_synonym = synonyms[0]
+        random_idx = random.randint(0, len(new_words)-1)
+        new_words.insert(random_idx, random_synonym)
+
+    def eda(self, sentence):
+        words = sentence.split(' ')
+        words = [word for word in words if word != '']
+        num_words = len(words)
+        augmented_sentences = []
+        num_new_per_technique = int(self.num_aug / 4) + 1
+
+        if self.alpha_sr > 0:
+            n_sr = max(1, int(self.alpha_sr * num_words))
+            for _ in range(num_new_per_technique):
+                a_words = self.synonym_replacement(words, n_sr)
+                augmented_sentences.append(' '.join(a_words))
+
+        if self.alpha_ri > 0:
+            n_ri = max(1, int(self.alpha_ri * num_words))
+            for _ in range(num_new_per_technique):
+                a_words = self.random_insertion(words, n_ri)
+                augmented_sentences.append(' '.join(a_words))
+
+        if self.alpha_rs > 0:
+            n_rs = max(1, int(self.alpha_rs * num_words))
+            for _ in range(num_new_per_technique):
+                a_words = self.random_swap(words, n_rs)
+                augmented_sentences.append(' '.join(a_words))
+
+        if self.p_rd > 0:
+            for _ in range(num_new_per_technique):
+                a_words = self.random_deletion(words, self.p_rd)
+                augmented_sentences.append(' '.join(a_words))
+
+        random.shuffle(augmented_sentences)
+
+        if self.num_aug >= 1:
+            augmented_sentences = augmented_sentences[:self.num_aug]
+        else:
+            keep_prob = self.num_aug / len(augmented_sentences)
+            augmented_sentences = [s for s in augmented_sentences if random.uniform(0, 1) < keep_prob]
+
+        augmented_sentences.append(sentence)
+        return augmented_sentences

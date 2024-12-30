d6245a7cae2e33b4fe5ea5289ece2cdb16e21209
Maldu <anamld62@gmail.com>
2024-12-30 15:41:13 +0100

    keyboardAug for 1.25x set and uploaded via gha

--- research/utils/experiments_utils.py
+++ research/utils/experiments_utils.py
@@ -58,4 +58,92 @@ def log_system_resources():
     logging.info("CPU Usage: %s%%", cpu)
 
 
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
 
+
+from sklearn.metrics import classification_report, balanced_accuracy_score, fbeta_score
+
+def print_classification_reports(y_train, y_train_pred, y_test, y_test_pred, target_names=['ham', 'spam']):
+    """
+    Generates and prints the classification reports for training and test data.
+
+    Parameters:
+        y_train (array-like): True labels for the training data.
+        y_train_pred (array-like): Predicted labels for the training data.
+        y_test (array-like): True labels for the test data.
+        y_test_pred (array-like): Predicted labels for the test data.
+        target_names (list, optional): Names of the target classes. Default is None.
+    """
+    train_report = classification_report(y_train, y_train_pred, target_names=target_names, digits=3)
+    print("Classification Report (Train Data):")
+    print(train_report)
+
+    test_report = classification_report(y_test, y_test_pred, target_names=target_names, digits=3)
+    print("Classification Report (Test Data):")
+    print(test_report)
+
+
+
+
+
+def plot_roc_curve(pipeline, X_train, y_train, X_test, y_test):
+    y_test_prob = pipeline.predict_proba(X_test)[:, 1]
+    y_train_prob = pipeline.predict_proba(X_train)[:, 1]
+
+    fpr_train, tpr_train, _ = roc_curve(y_train, y_train_prob)
+    fpr_test, tpr_test, _ = roc_curve(y_test, y_test_prob)
+
+    auc_train = auc(fpr_train, tpr_train)
+    auc_test = auc(fpr_test, tpr_test)
+
+    plt.figure(figsize=(5, 5))
+    plt.plot(fpr_train, tpr_train, label=f'Train ROC (AUC = {auc_train:.3f})', color='blue', linewidth=2)
+    plt.plot(fpr_test, tpr_test, label=f'Test ROC (AUC = {auc_test:.3f})', color='red', linestyle='--', linewidth=2)
+    plt.plot([0, 1], [0, 1], color='gray', linestyle='--', linewidth=1, label='Random classifier')
+    plt.title('ROC Curve', fontsize=16)
+    plt.xlabel('False Positive Rate (FPR)', fontsize=12)
+    plt.ylabel('True Positive Rate (TPR)', fontsize=12)
+    plt.legend(loc='lower right', fontsize=12)
+    plt.grid(alpha=0.3)
+    plt.tight_layout()
+    plt.show()
+
+
+
+    
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+
+
+def plot_confusion_matrix(y_test, y_test_pred):
+    test_conf_matrix = confusion_matrix(y_test, y_test_pred)
+    matrix_fig, ax = plt.subplots(figsize=(5, 5))
+    cm_display = ConfusionMatrixDisplay(confusion_matrix=test_conf_matrix, display_labels=['Ham', 'Spam'])
+    cm_display.plot(cmap='Blues', ax=ax)
+    plt.title("Confusion Matrix")
+    return matrix_fig
+    
+    
+    
+    
+    
+from sklearn.metrics import precision_recall_curve as pr_curve
+
+def plot_precision_recall_curve(pipeline, X_test, y_test):
+    # Get predicted probabilities for class 1
+    y_test_pred_prob = pipeline.predict_proba(X_test)[:, 1]
+
+    # Calculate precision, recall, and thresholds
+    precision, recall, _ = pr_curve(y_test, y_test_pred_prob)
+    pr_auc = auc(recall, precision)
+
+    # Plot Precision-Recall curve
+    pr_fig, ax = plt.subplots(figsize=(5, 5))  
+    ax.plot(recall, precision, color='b', label=f'PR AUC = {pr_auc:.3f}')
+    ax.set_xlabel('Recall')
+    ax.set_ylabel('Precision')
+    ax.set_title('Precision-Recall Curve')
+    ax.legend(loc="best")
+    ax.grid(True)
+    # plt.show()
+    return pr_fig

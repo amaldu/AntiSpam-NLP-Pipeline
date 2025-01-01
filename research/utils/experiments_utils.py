dd65ff0d50c18b0d37d0ca449458e888d7e2edeb
Maldu <anamld62@gmail.com>
2025-01-01 21:17:01 +0100

    function for highlighting changed words included in experimentation_utils

--- research/utils/experiments_utils.py
+++ research/utils/experiments_utils.py
@@ -4,7 +4,7 @@ import yaml
 import psutil
 
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def experiment_status(config_file='experiments_config.yaml'):
     """
@@ -147,3 +147,23 @@ def plot_precision_recall_curve(pipeline, X_test, y_test):
     ax.grid(True)
     # plt.show()
     return pr_fig
+
+
+from colorama import Fore
+
+
+def print_and_highlight_diff(orig_text, new_texts):
+    """ A simple diff viewer for augmented texts. """
+    for orig_text, new_text in zip(orig_text, new_texts):
+        orig_split = orig_text.split()
+        print("-"*50) 
+        print(f"Original: {len(orig_split)}\n{orig_text}")
+        print(f"Augmented: {len(new_text.split())}")
+        
+        for i, word in enumerate(new_text.split()):
+            if i < len(orig_split) and word == orig_split[i]:
+                print(word, end=" ")
+            else:
+                print(Fore.RED + word + Fore.RESET, end=" ")
+                
+        print()

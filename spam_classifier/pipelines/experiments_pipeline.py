c45786e10ca01933806d5e0cde5d4358a3520ea5
Maldu <anamld62@gmail.com>
2025-01-06 19:19:15 +0100

    change import vectorizers

--- spam_classifier/pipelines/experiments_pipeline.py
+++ spam_classifier/pipelines/experiments_pipeline.py
@@ -4,14 +4,16 @@ from sklearn.metrics import classification_report
 from spam_classifier.components.data_ingestion import read_dataset
 from spam_classifier.components.data_preprocessing import clean_types_duplicates, preprocess
 from spam_classifier.components.split_data import train_test_val_split
-from spam_classifier.components.feature_engineering import BowFeatureExtractor, TfidfFeatureExtractor
+from spam_classifier.components.vectorizers import BowFeatureExtractor, TfidfFeatureExtractor
 from spam_classifier.components.models import MultinomialNaiveBayesClassifier, LogisticRegressionClassifier
 
+
+logging.basicConfig(filename='logs/app.log', level=logging.INFO)
 def baseline_experiment_v1():
     config_path = "experiments_config.yaml"
     try:
         logging.info("Loading dataset...")
-        data = read_dataset(config_path) 
+        data = read_dataset(config_path)
         
         logging.info("Cleaning types and duplicates...")
         df_cleaned = clean_types_duplicates(data)
@@ -21,7 +23,6 @@ def baseline_experiment_v1():
         
         logging.info("Splitting into train, test and validation sets...")
         X_train, X_val, X_test, y_train, y_val, y_test = train_test_val_split(df_preprocessed)
-
         
         logging.info("Applying pipeline...")
         pipeline = Pipeline([
@@ -34,15 +35,14 @@ def baseline_experiment_v1():
         y_test_pred = pipeline.predict(X_test)
         
         return y_train_pred, y_test_pred
-        
-        
-        
-
 
     except Exception as e:
         logging.error(f"baseline_experiment_v1: {e}")
 
-if __name__ == "__main__":
+def main():
     y_train_pred, y_test_pred = baseline_experiment_v1()
     print("Train predictions:", y_train_pred)
     print("Test predictions:", y_test_pred)
+
+if __name__ == "__main__":
+    main()

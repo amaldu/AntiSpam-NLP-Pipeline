ba5805d133c2e0088932468d1186bbb3a830d512
Maldu <anamld62@gmail.com>
2025-01-07 11:02:27 +0100

    added config uploading to read_dataset function

--- spam_classifier/components/data_ingestion.py
+++ spam_classifier/components/data_ingestion.py
@@ -7,48 +7,27 @@ import yaml
 import logging
 import exception
 
-
-def load_config(config_path):
+def read_dataset(config_path: str) -> pd.DataFrame:
+    logging.info(f"Loading config from {config_path}")
     try:
         with open(config_path, 'r') as file:
             config = yaml.safe_load(file)
-            return config
 
-    except FileNotFoundError:
-        raise FileNotFoundError(f"The config file {config_path} was not found")
-    except yaml.YAMLError as e:
-        raise ValueError(f"Error reading the YAML: {e}")
+        dataset_dir = os.path.abspath(config['dataset']['path'])
+        dataset_file = config['dataset']['file']
+        dataset_path = os.path.join(dataset_dir, dataset_file)
 
-def read_dataset(config_path: str) -> pd.DataFrame:
-    if not os.path.isabs(config_path):
-        config_path = os.path.join(os.path.dirname(__file__), '../pipelines/experiments_config.yaml')
-
-    logging.info(f"Loading config from {config_path}")
-    
-    config = load_config(config_path)
-    base_path = config['path']
-    dataset_file = config['file']
-    logging.info(f"Loading base_path from {base_path}")
-    logging.info(f"Loading dataset_file from {dataset_file}")
-    dataset_path = os.path.join(base_path, dataset_file)
-
-    logging.info(f"Reading the dataset from {dataset_path}")
-    
-    try:
         if not os.path.exists(dataset_path):
             raise FileNotFoundError(f"Dataset '{dataset_path}' does not exist.")
-        
+
         data = pd.read_csv(dataset_path)
 
         if data.empty:
             raise ValueError(f"The dataset '{dataset_path}' is empty or malformed.")
-        
+
+        logging.info(f"Dataset successfully loaded from {dataset_path} with shape: {data.shape}")
         return data
 
     except Exception as e:
         logging.error(f"Error reading the data: {e}")
-        raise ValueError(f"An error occurred while reading the dataset '{dataset_path}': {e}")
-
-
-
-
+        raise ValueError(f"An error occurred while reading the dataset: {e}")
